24aa074f2bd6fe6cd91f3d751fc49206b9d73847
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-11-22 14:00:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+//remove the <main> with id 'main'
+document.getElementById("main").remove() 
+
+//create an <h1> and assign it to the 'newHeader' variable
+var newHeader = document.querySelector("h1")
+
