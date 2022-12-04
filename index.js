a9ce0c73407fada817103c21f9883ef79d2b6035
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-12-04 23:53:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //remove the <main> with id 'main'
 document.getElementById("main").remove() 
 
-//create an <h1> and assign it to the 'newHeader' variable
-var newHeader = document.querySelector("h1")
+//create an <h1> and assign it to the 'newHeader' variable with id = victory and text = vanessa is the championn'
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Vanessa is the champion'
 
